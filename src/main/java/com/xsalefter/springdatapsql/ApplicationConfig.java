a1fe55e4dc8535d710b03f98a99de4de39e9d90a
xsalefter <xsalefter@gmail.com>
2016-09-01 12:07:39 +0700

    More neat configuration.

--- src/main/java/com/xsalefter/springdatapsql/ApplicationConfig.java
+++ src/main/java/com/xsalefter/springdatapsql/ApplicationConfig.java
@@ -6,15 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 @EnableAutoConfiguration
@@ -25,18 +21,27 @@ public class ApplicationConfig {
     private final Logger LOG = LoggerFactory.getLogger(ApplicationConfig.class);
 
 
+    /*
     @Bean
     @ConfigurationProperties(prefix="spring.datasource")
-    public DataSource dataSource(DataSourceProperties dsProperties) {
-//        LOG.debug(">> Creating new DataSource with properties: {}", dsProperties);
-//
-//        final HikariConfig config = new HikariConfig();
-//        config.setDataSourceClassName(dsProperties.getType().getName());
-//        config.addDataSourceProperty("url", dsProperties.getUrl());
-//        config.addDataSourceProperty("user", dsProperties.getUsername());
-//        config.addDataSourceProperty("password", dsProperties.getPassword());
-//
-//        return new HikariDataSource(config);
-    	return DataSourceBuilder.create().build();
+    public DataSource dataSource(org.springframework.boot.autoconfigure.jdbc.DataSourceProperties dsProperties) {
+        LOG.debug(">> Creating new DataSource with properties: {}", dsProperties);
+
+        final com.zaxxer.hikari.HikariConfig config = new com.zaxxer.hikari.HikariConfig();
+        config.setDataSourceClassName(dsProperties.getType().getName());
+        config.addDataSourceProperty("url", dsProperties.getUrl());
+        config.addDataSourceProperty("user", dsProperties.getUsername());
+        config.addDataSourceProperty("password", dsProperties.getPassword());
+
+        return new com.zaxxer.hikari.HikariDataSource(config);
+    }
+    */
+
+
+    @Bean
+    @ConfigurationProperties(prefix="spring.datasource")
+    public DataSource dataSource() {
+        LOG.debug(">> Creating new DataSource with default configuration.");
+        return DataSourceBuilder.create().build();
     }
 }
