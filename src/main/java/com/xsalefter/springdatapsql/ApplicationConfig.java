307235b11440c07bcc427671aabc7337648369da
xsalefter <xsalefter@gmail.com>
2016-09-01 10:58:20 +0700

    Use DataSourceBuilder for auto configuration.

--- src/main/java/com/xsalefter/springdatapsql/ApplicationConfig.java
+++ src/main/java/com/xsalefter/springdatapsql/ApplicationConfig.java
@@ -5,7 +5,9 @@ import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -24,15 +26,17 @@ public class ApplicationConfig {
 
 
     @Bean
-    public DataSource dataSource(final DataSourceProperties dsProperties) {
-        LOG.debug(">> Creating new DataSource with properties: {}", dsProperties);
-
-        final HikariConfig config = new HikariConfig();
-        config.setDataSourceClassName(dsProperties.getType().getName());
-        config.addDataSourceProperty("url", dsProperties.getUrl());
-        config.addDataSourceProperty("user", dsProperties.getUsername());
-        config.addDataSourceProperty("password", dsProperties.getPassword());
-
-        return new HikariDataSource(config);
+    @ConfigurationProperties(prefix="spring.datasource")
+    public DataSource dataSource(DataSourceProperties dsProperties) {
+//        LOG.debug(">> Creating new DataSource with properties: {}", dsProperties);
+//
+//        final HikariConfig config = new HikariConfig();
+//        config.setDataSourceClassName(dsProperties.getType().getName());
+//        config.addDataSourceProperty("url", dsProperties.getUrl());
+//        config.addDataSourceProperty("user", dsProperties.getUsername());
+//        config.addDataSourceProperty("password", dsProperties.getPassword());
+//
+//        return new HikariDataSource(config);
+    	return DataSourceBuilder.create().build();
     }
 }
